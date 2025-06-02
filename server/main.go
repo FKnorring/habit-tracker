eb2fab05db91fe46db18d032f725161804bf18d0
FKnorring <fille.vk@gmail.com>
2025-06-02 15:44:30 +0200

    added data models

--- server/main.go
+++ server/main.go
@@ -6,6 +6,36 @@ import (
 )
 
 /*
+	Habit:
+		id
+		name
+		description
+		frequency
+		startDate
+
+	Tracking:
+		id
+		habitId
+		timestamp
+		note?
+*/
+
+type Habit struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Frequency   string `json:"frequency"`
+	StartDate   string `json:"startDate"`
+}
+
+type Tracking struct {
+	ID        string `json:"id"`
+	HabitID   string `json:"habitId"`
+	Timestamp string `json:"timestamp"`
+	Note      string `json:"note"`
+}
+
+/*
 	Endpoints:
 		GET /habits
 		GET /habits/:id
